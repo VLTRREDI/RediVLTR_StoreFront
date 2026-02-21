66b024966b5949d02674bb7d70c26bc769b43d44
VLTRREDI <volturiastaff@gmail.com>
2026-02-21 13:54:19 +0100

    update

---
+++ Redi-VolturiaStore-storefront/src/app/api/revalidate/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server"
+import { revalidatePath } from "next/cache"
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams
+    const tags = searchParams.get("tags") as string
+
+    if (!tags) {
+        return NextResponse.json({ error: "No tags provided" }, { status: 400 })
+    }
+
+    const tagsArray = tags.split(",")
+    await Promise.all(
+        tagsArray.map(async (tag) => {
+            switch (tag) {
+                case "products":
+                    revalidatePath("/[countryCode]/(main)/store", "page")
+                    revalidatePath("/[countryCode]/(main)/products/[handle]", "page")
+            }
+        })
+    )
+
+    return NextResponse.json({ message: "Revalidated" }, { status: 200 })
+}
